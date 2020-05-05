e0e40b0de44159763ae3a28cc9c641a2b76c5ea9
kirtcathey <kirtcathey@sysrisk.com>
2020-05-05 19:39:14 +0900

    Committer: root <root@localhost.localdomain> On branch master Changes to be committed: 	modified:   DaBomb.py
    Untracked files:
    	443_util_Report.pdf
    	BAD_Report.pdf
    	Report_032-444.pdf
    	Report_443.pdf
    	Report_4444.pdf
    
    On branch master
    Changes to be committed:
    	modified:   DaBomb.py
    
     Untracked files:
    	443_util_Report.pdf
    	BAD_Report.pdf
    	Report_032-444.pdf
    	Report_443.pdf
    	Report_4444.pdf

--- DaBomb.py
+++ DaBomb.py
@@ -18,9 +18,8 @@ class bcolors:
 
 
 def bomb():
-    os.system('clear')
-    print
-    bcolors.OKGREEN + '''
+    #os.system('clear')
+    print bcolors.OKGREEN + '''
 			 \|/
                        `--+--'
                           |
@@ -28,11 +27,11 @@ def bomb():
                       |#######|
                    _.-'#######`-._
                 ,-'###############`-.
-              ,'#####################`,         .___     .__         .
-             |#########################|        [__ ._ _ [__) _ ._ _ |_  _ ._.
-            |###########################|       [___[ | )[__)(_)[ | )[_)(/,[
+              ,'#####################`,
+             |#########################|
+            |###########################|
            |#############################|
-           |#############################|              Author: Mazen Elzanaty
+           |#############################|
            |#############################|
             |###########################|
              \#########################/
@@ -42,8 +41,11 @@ def bomb():
 *.______________________________________________________________,' (Bomb)
                                                                     `--' ''' + bcolors.ENDC
 
+# print "Bomb ran..."
+#os.system('clear')
+#print "cleared..."
+bomb()
 
-os.system('clear')
 try:
     file1 = open('Banner.txt', 'r')
     print(' ')
@@ -55,27 +57,28 @@ except IOError:
 
 # Input
 print(bcolors.WARNING + '''
-Choose a Mail Service:
+Choose an SMTP Relay:
 1) Gmail
 2) Yahoo
 3) Hotmail/Outlook
-4) SMTP Open Relay
+4) Other SMTP Relay
 ''' + bcolors.ENDC + '--------------------------------------------------------------')
 try:
     server = raw_input(bcolors.OKGREEN + 'Mail Server: ' + bcolors.ENDC)
-    user = raw_input(bcolors.OKGREEN + 'Your Email: ' + bcolors.ENDC)
-    pwd = getpass.getpass(bcolors.OKGREEN + 'Password: ' + bcolors.ENDC)
+    user = raw_input(bcolors.OKGREEN + 'Login Email: ' + bcolors.ENDC)
+    pwd = getpass.getpass(bcolors.OKGREEN + 'Login Password: ' + bcolors.ENDC)
+    spoof = raw_input(bcolors.OKGREEN + 'From Email Address: ' + bcolors.ENDC) 
     to = raw_input(bcolors.OKGREEN + 'To: ' + bcolors.ENDC)
     subject = raw_input(bcolors.OKGREEN + 'Subject (Optional): ' + bcolors.ENDC)
     body = raw_input(bcolors.OKGREEN + 'Message: ' + bcolors.ENDC)
-    nomes = input(bcolors.OKGREEN + 'Number of Emails to send: ' + bcolors.ENDC)
-    no = 0
+    #nomes = input(bcolors.OKGREEN + 'Number of Emails to send: ' + bcolors.ENDC)
+    #no = 0
 # Attachments
-    attach = raw_input('Add an attachment? <y/n> ')
+    attach = raw_input(bcolors.OKGREEN + 'Add an attachment? <y/n> ' + bcolors.ENDC)
     if attach == 'y':
         message = MIMEMultipart()
         # storing the senders email address
-        message['From'] = user
+        message['From'] = spoof
         # storing the receivers email address
         message['To'] = to
         # storing the subject
@@ -90,7 +93,7 @@ try:
 
         # open the file to be sent
         filename = "File_name_with_extension"
-        filepath = raw_input('Please input the full path to the file: ')
+        filepath = raw_input(bcolors.OKGREEN + 'Please input the full path to the file: ' + bcolors.ENDC)
         attachment = open(filepath, "rb")
 
         # instance of MIMEBase and named as p
@@ -106,138 +109,136 @@ try:
 
         # attach the instance 'p' to instance 'msg'
         message.attach(p)
+        print "message created"
+        
     else:
-        message = 'From: ' + user + '\nSubject: ' + subject + '\n' + body
+        message = 'From: ' + spoof + '\nSubject: ' + subject + '\n' + body
 except KeyboardInterrupt:
-    print
-    bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+    print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
     sys.exit()
 
 # Gmail
 
 if server == '1' or server == 'gmail' or server == 'Gmail':
+    smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
     bomb()
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.ehlo()
-    server.starttls()
+    smtp_server.ehlo()
+    smtp_server.starttls()
     try:
-        server.login(user, pwd)
+        smtp_server.login(user, pwd)
     except smtplib.SMTPAuthenticationError:
-        print
-        bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+        print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
 		Or you need to enable less secure apps
 		On Gmail: https://myaccount.google.com/lesssecureapps ''' + bcolors.ENDC
         sys.exit()
-    while no != nomes:
+    #while no != nomes:
         try:
-            server.sendmail(user, to, message)
-            print
-            bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
-            no += 1
+            smtp_server.sendmail(user, to, message)
+            #print
+            #bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+            #no += 1
+            print bcolors.WARNING + 'Successfully sent emails' + bcolors.ENDC
             time.sleep(.8)
         except KeyboardInterrupt:
-            print
-            bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+            print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
             sys.exit()
         except:
-            print
-            "Failed to Send "
-    server.close()
+            print "Failed to Send "
+    smtp_server.close()
 
 # Yahoo
 elif server == '2' or server == 'Yahoo' or server == 'yahoo':
-    server = smtplib.SMTP("smtp.mail.yahoo.com", 587)
+    smtp_server = smtplib.SMTP("smtp.mail.yahoo.com", 587)
     bomb()
-    server.starttls()
+    smtp_server.starttls()
     try:
-        server.login(user, pwd)
+        smtp_serverlogin(user, pwd)
     except smtplib.SMTPAuthenticationError:
-        print
-        bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
+        print bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
 		Or you need to enable less secure apps
 		On Yahoo: https://login.yahoo.com/account/security?.scrumb=Tiby8TXUvJt#less-secure-apps
 		''' + bcolors.ENDC
         sys.exit()
-    while no != nomes:
+    #while no != nomes:
         try:
-            server.sendmail(user, to, message)
-            print
-            bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
-            no += 1
+            smtp_server.sendmail(user, to, message)
+            #print
+            #bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+            #no += 1
+            print bcolors.WARNING + 'Successfully sent emails' + bcolors.ENDC
             time.sleep(.8)
         except KeyboardInterrupt:
-            print
-            bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+            print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
             sys.exit()
         except:
-            print
-            "Failed to Send"
-    server.close()
+            print "Failed to Send"
+    smtp_server.close()
 
 # Hotmail/Outlook
 elif server == '3' or server == 'outlook' or server == 'Outlook' or server == 'Hotmail' or server == 'hotmail':
-    server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+    smtp_server = smtplib.SMTP("smtp-mail.outlook.com", 587)
     bomb()
-    server.ehlo()
-    server.starttls()
+    smtp_server.ehlo()
+    smtp_server.starttls()
     try:
-        server.login(user, pwd)
+        smtp_server.login(user, pwd)
     except smtplib.SMTPAuthenticationError:
-        print
-        bcolors.FAIL + 'Your Username or Password is incorrect, please try again using the correct credentials' + bcolors.ENDC
+        print bcolors.FAIL + 'Your Username or Password is incorrect, please try again using the correct credentials' + bcolors.ENDC
         sys.exit()
-    while no != nomes:
+    #while no != nomes:
         try:
-            server.sendmail(user, to, message)
-            print
-            bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
-            no += 1
+            smtp_server.sendmail(user, to, message)
+            #print
+            #bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+            #no += 1
+            print bcolors.WARNING + 'Successfully sent emails' + bcolors.ENDC
             time.sleep(.8)
         except KeyboardInterrupt:
-            print
-            bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+            print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
             sys.exit()
         except smtplib.SMTPAuthenticationError:
-            print
-            '\nThe username or password you entered is incorrect.'
+            print '\nThe username or password you entered is incorrect.'
             sys.exit()
         except:
-            print
-            "Failed to Send "
-    server.close()
+            print "Failed to Send "
+    smtp_server.close()
 
 # SMTP Open Relay
 elif server == '4' or server == 'smtp' or server == 'SMTP':
     relaysrvr = raw_input('Input smtp server ip or server name; ex: mail.domain.com: ')
     srvrport = raw_input('Input smtp server port - usually 25: ')
-    server = smtplib.SMTP(relaysrvr, srvrport)
-    bomb()
-    server.starttls()
+    print "Connecting to the server..."
+    smtp_server = smtplib.SMTP(relaysrvr, srvrport)
+    print "Got this far...."
+    #bomb()
+    #server.starttls()
     try:
-        server.login(user, pwd)
+		print "Now authenticate..."
+		smtp_server.login(user, pwd)
+		print "Authenticated..."
+		print "Sending message ..."
+		try:
+			smtp_server.sendmail(user, to, message.as_string())
+		
+			#print
+			#bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
+			#no += 1
+			bomb()
+			print bcolors.WARNING + 'Successfully sent emails' + bcolors.ENDC
+			time.sleep(.8)
+			smtp_server.close()
+		except KeyboardInterrupt:
+			print bcolors.FAIL + '\nCanceled' + bcolors.ENDC
+			smtp_server.close()
+			sys.exit()
+		except:
+			print "Failed to send..."
+			sys.exit()
     except smtplib.SMTPAuthenticationError:
-        print
-        bcolors.FAIL + '''Your Username or Password is incorrect, please try again using the correct credentials
-			Or you need to find another relay...
-			''' + bcolors.ENDC
-        sys.exit()
-    while no != nomes:
-        try:
-            server.sendmail(user, to, message)
-            print
-            bcolors.WARNING + 'Successfully sent ' + str(no + 1) + ' emails' + bcolors.ENDC
-            no += 1
-            time.sleep(.8)
-        except KeyboardInterrupt:
-            print
-            bcolors.FAIL + '\nCanceled' + bcolors.ENDC
-            sys.exit()
-        except:
-            print
-            "Failed to Send"
-    server.close()
+        print bcolors.FAIL + 'Your Username or Password is incorrect, please try again using the correct credentials. Or you need to find another relay...' + bcolors.ENDC
+        sys.exit()	
+	smtp_server.close()
 
 else:
-    print
-    'Works with Gmail, Yahoo, Outlook Hotmail, and now, SMTP Open Relay!'
+    print 'Works with Gmail, Yahoo, Outlook Hotmail, and now, SMTP Open Relay!'
     sys.exit()
